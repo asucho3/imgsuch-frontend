285a3f363fe5155b5bcb875dd53d43629814c549
Alan Suchowolski <asucho3@gmail.com>
2023-07-04 15:09:22 -0300

    minor fixes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -8,31 +8,29 @@ function ProtectedRoute({ children }) {
   const [checkAuth, setCheckAuth] = useState(false);
   const navigate = useNavigate();
 
-  //   useEffect(
-  //     function () {
-  //       async function isLoggedIn() {
-  //         try {
-  //           setCheckAuth(true);
-  //           const data = await checkLoggedIn();
-  //           if (data.status === "success") {
-  //             dispatch({ type: "user/isLoggedIn" });
-  //           } else {
-  //             // dispatch({ type: "user/logout" });
-  //             navigate("/");
-  //           }
-  //           setCheckAuth(false);
-  //         } catch (err) {
-  //           console.log(err);
-  //         }
-  //       }
-  //       isLoggedIn();
-  //     },
-  //     [dispatch, navigate, setCheckAuth]
-  //   );
+  useEffect(
+    function () {
+      async function isLoggedIn() {
+        try {
+          setCheckAuth(true);
+          const data = await checkLoggedIn();
+          if (data.status === "success") {
+            dispatch({ type: "user/isLoggedIn" });
+          } else {
+            dispatch({ type: "user/logout" });
+            navigate("/");
+          }
+          setCheckAuth(false);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      isLoggedIn();
+    },
+    [dispatch, navigate, setCheckAuth]
+  );
 
-  //   return isAuthenticated ? children : null;
-  // }
-  return null;
+  return isAuthenticated ? children : null;
 }
 
 export default ProtectedRoute;
